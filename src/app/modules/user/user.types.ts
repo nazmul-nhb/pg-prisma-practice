672f02844c827205ca9597559a8769505e3d9010
Nazmul Hassan <nazmulnhb007@yahoo.com>
2025-11-20 17:50:20 +0600

    `🔧 update(user):` updated user types

--- src/app/modules/user/user.types.ts
+++ src/app/modules/user/user.types.ts
@@ -1,14 +1,16 @@
-import type { User } from '@/configs/prisma';
+import type { Prisma, User } from '@/configs/prisma';
 import type { TEmail } from '@/types';
 import type { Prettify } from 'nhb-toolbox/utils/types';
 
 type Email = { email: TEmail };
 
 export type InsertUser = Prettify<
-	Omit<User, 'id' | 'created_at' | 'updated_at' | 'email'> & Email
+	Pick<Prisma.UserCreateInput, 'first_name' | 'last_name' | 'password'> & Email
 >;
 
-export type UpdateUser = Partial<Pick<InsertUser, 'first_name' | 'last_name' | 'user_name'>>;
+export type UpdateUser = Partial<
+	Pick<Prisma.UserCreateInput, 'first_name' | 'last_name' | 'user_name'>
+>;
 
 export type TUser = Prettify<Omit<User, 'email'> & Email>;
 
